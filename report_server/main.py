ce29cc5965668238474c0507ecd61446d18125b9
Robertleoj <robert.leo.jonsson@gmail.com>
2022-06-06 16:51:52 +0000

    Return json

--- report_server/main.py
+++ report_server/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import HTMLResponse
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 REPORT_DIR = './reports'
-
 
 class ReportReq(BaseModel):
     report_id: str
 
 app = FastAPI()
 
-@app.post('/run_report', response_class=HTMLResponse)
+class ReportResponse(BaseModel):
+    html: str
+
+@app.post('/run_report', response_class=JSONResponse)
 async def run_report(param: ReportReq):
     with open(f'{REPORT_DIR}/{param.report_id}.html', 'r') as f:
-        return HTMLResponse(f.read())
+        html = f.read()
+    res = ReportResponse(html=html)
+    return JSONResponse(content=jsonable_encoder(res))
