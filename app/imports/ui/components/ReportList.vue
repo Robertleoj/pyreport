768c634fa36a301cb96be7afd9206d2e7bc32476
Robertleoj <robert.leo.jonsson@gmail.com>
2022-06-06 14:25:36 +0000

    add .meteor to repo

---
+++ app/imports/ui/components/ReportList.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <ReportListItem
+            v-for="report in reports"
+            :key="report._id"
+            :reportId="report._id"
+        />
+    </div>
+</template>
+
+<script>
+import Reports from '../../api/collections/Reports';
+import ReportListItem from './ReportListItem.vue'
+
+export default {
+    data(){
+        return {}
+    },
+
+    components: {
+        ReportListItem,
+    },
+
+    meteor: {
+        $subscribe: {
+            'reports': [],
+        },
+        reports() {
+            console.log("getting reports");
+            console.log(Reports.find({}).fetch());
+            return Reports.find({});
+        }
+    },
+
+}
+
+</script>
