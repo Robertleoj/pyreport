b2227819042504c9b5b02c8d14ebda4a3ab9bdb5
DongusJr <bjarkim19@ru.is>
2022-06-07 00:10:19 +0000

    Trying to add codemirror

--- imports/ui/components/Terminal/TerminalPage.vue
+++ imports/ui/components/Terminal/TerminalPage.vue
@@ -1,13 +1,38 @@
 <template>
-    <div>
-        Hello World
-    </div>
+  <codemirror
+    v-model="code"
+    placeholder="Code goes here..."
+    :style="{ height: '400px' }"
+    :autofocus="true"
+    :indent-with-tab="true"
+    :tabSize="2"
+    :extensions="extensions"
+    @ready="log('ready', $event)"
+    @change="log('change', $event)"
+    @focus="log('focus', $event)"
+    @blur="log('blur', $event)"
+  />
 </template>
 
+
+
 <script>
+import {Codemirror} from 'vue-codemirror'
+import {javascript} from '@codemirror/lang-javascript'
+import { oneDark} from '@codemirror/theme-one-dark'
+
 export default {
-    data(){
-        return {}
+    components: {
+        Codemirror
+    },
+    setup() {
+        const code = ref(`console.log('Hello World!')`)
+        const extensions = [javascript(), oneDark]
+        return {
+            code,
+            extensions,
+            log: console.log
+        }
     },
 }
 </script>
